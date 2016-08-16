10fac7bb1f9fba8fa6470a19e79304fb4a24f1c8
ImTheBirdMan <browner6009@gmail.com>
2016-08-16 16:55:43 -0500

    Add files via upload

---
+++ Datagram.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.net.SocketException;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+
+public class Datagram {
+   public static String mServiceName = "JmDNS Server";
+   public static final String SERVICE_TYPE = "_IAmTheBirdman._tcp.local";
+   
+   public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+      DatagramSocket socket = new DatagramSocket();
+      SocketAddress addr = socket.getLocalSocketAddress(); 
+      socket.bind(addr);
+      byte[] buf = new byte[64];
+      DatagramPacket packet = new DatagramPacket(buf, buf.length);
+      int mPort = socket.getPort();
+      JmDNS jmdns = JmDNS.create();
+      ServiceInfo info = ServiceInfo.create(SERVICE_TYPE, mServiceName, mPort, "App service");
+      jmdns.unregisterAllServices();
+      jmdns.registerService(info);
+      System.out.println("REGISTERED");
+   
+      
+      if (socket.isConnected() == true){
+         while (!socket.isClosed()==false) {
+            socket.receive(packet);
+            String data = new String(packet.getData());
+            String[] dataParsed = data.split(",");
+            float timestamp = Float.parseFloat(dataParsed[0]);
+            float sensortype = Float.parseFloat(dataParsed[1]);
+            float x = Float.parseFloat(dataParsed[2]);
+            float y = Float.parseFloat(dataParsed[3]);
+            float z = Float.parseFloat(dataParsed[4]);
+            String sensorname = new String();
+            if (sensortype == 1) {
+               sensorname = "GPS";
+            }
+            else if (sensortype == 2) {
+               sensorname = "Magnetometer";
+            }
+            else if (sensortype == 3) {
+               sensorname = "Accelerometer";
+            }
+            else if (sensortype == 4) {
+               sensorname = "Gyroscope";
+            }
+            else if (true) {
+               sensorname = "Unknown";
+            }
+            Date date = new Date();String line = ("Timestamp " + timestamp + ", local date " + date.toString() + ", sensor " + sensorname + ", x " + x + ", y " + y + ", z " + z);
+            System.out.println(line);
+         
+         }
+      }
+      
+   }
+}
