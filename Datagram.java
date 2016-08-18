3a3d53bd9f3f65870dd47599c8382c32c1ea7f44
ImTheBirdMan <browner6009@gmail.com>
2016-08-17 19:41:52 -0500

    Add files via upload

--- Datagram.java
+++ Datagram.java
@@ -9,57 +9,109 @@ import java.net.*;
 import java.net.SocketException;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+import javax.swing.JFrame;
+import javax.swing.JPanel; 
+import javax.swing.JComboBox; 
+import javax.swing.JButton; 
+import javax.swing.JLabel; 
+import javax.swing.JList; 
+import java.awt.BorderLayout; 
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
-
-public class Datagram {
+public class Datagram extends Gui{
    public static String mServiceName = "JmDNS Server";
-   public static final String SERVICE_TYPE = "_IAmTheBirdman._tcp.local";
+   public static final String SERVICE_TYPE = "_IAmTheBirdman._udp.local";
    
    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+        
+        
+      new Gui(); 
+    
+      
+      
+      
       DatagramSocket socket = new DatagramSocket();
-      SocketAddress addr = socket.getLocalSocketAddress(); 
-      socket.bind(addr);
+      SocketAddress addr = socket.getLocalSocketAddress();
+      System.out.println(addr);
+   
       byte[] buf = new byte[64];
       DatagramPacket packet = new DatagramPacket(buf, buf.length);
-      int mPort = socket.getPort();
+      int mPort = socket.getLocalPort();
+      System.out.println(mPort);
       JmDNS jmdns = JmDNS.create();
       ServiceInfo info = ServiceInfo.create(SERVICE_TYPE, mServiceName, mPort, "App service");
-      jmdns.unregisterAllServices();
+   
       jmdns.registerService(info);
       System.out.println("REGISTERED");
+      System.out.println("Address:" + InetAddress.getLocalHost().getHostAddress());
    
+      System.out.println("connected?");
+      System.out.println(socket.isConnected());
+   
+      System.out.println("Socket is closed?");
+      System.out.println(socket.isClosed());
+   
+      System.out.println("Socket is bound?");
+      System.out.println(socket.isBound());
+      int xlast = 0;
+      int x = xlast;
+   
+      int ylast = 0;
+      int y = ylast;
+   
+      int zlast = 0;
+      int z = zlast;
+   
+      int thetalast = 0;
+      int theta = thetalast;
+   
+        //while(true){
+      while (!socket.isClosed()) {
+         socket.receive(packet);
+         String data = new String(packet.getData()); // returns buf?
       
-      if (socket.isConnected() == true){
-         while (!socket.isClosed()==false) {
-            socket.receive(packet);
-            String data = new String(packet.getData());
-            String[] dataParsed = data.split(",");
-            float timestamp = Float.parseFloat(dataParsed[0]);
-            float sensortype = Float.parseFloat(dataParsed[1]);
-            float x = Float.parseFloat(dataParsed[2]);
-            float y = Float.parseFloat(dataParsed[3]);
-            float z = Float.parseFloat(dataParsed[4]);
-            String sensorname = new String();
-            if (sensortype == 1) {
-               sensorname = "GPS";
-            }
-            else if (sensortype == 2) {
-               sensorname = "Magnetometer";
-            }
-            else if (sensortype == 3) {
-               sensorname = "Accelerometer";
-            }
-            else if (sensortype == 4) {
-               sensorname = "Gyroscope";
-            }
-            else if (true) {
-               sensorname = "Unknown";
-            }
-            Date date = new Date();String line = ("Timestamp " + timestamp + ", local date " + date.toString() + ", sensor " + sensorname + ", x " + x + ", y " + y + ", z " + z);
-            System.out.println(line);
-         
+             /* *
+              * PACKET FORMAT
+              * String
+              * "int(x),int(y),int(z),int(theta)"
+              *
+              * */
+            //System.out.println("Raw data: " + data);
+      
+         String[] dataParsed = data.split(":");
+         try {
+                // good packet
+            x = Integer.parseInt(dataParsed[0]);
+            xlast = x;
+            y = Integer.parseInt(dataParsed[1]);
+            ylast = y;
+            z = Integer.parseInt(dataParsed[2]);
+            zlast = z;
+            theta = Integer.parseInt(dataParsed[3].trim());
+            thetalast = theta;
+                // good packet
+         } 
+         catch (java.lang.NumberFormatException e) {
+                //corrupted packet;
+            x = xlast;
+            y = ylast;
+            z = zlast;
+            theta = thetalast;
          }
-      }
       
+         String line = ("\n          x:" + x +
+                            "\n          y:" + y +
+                            "\n          z:" + z +
+                            "\norientation:" + theta);
+         System.out.println(line);
+      }
+      //}
+      System.out.print("Enter any character to stop server: ");
+       // Read the char
+      char ch = (char) System.in.read();
+      jmdns.unregisterAllServices();
+      jmdns.unregisterAllServices();
    }
 }
